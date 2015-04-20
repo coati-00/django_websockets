e0dc7c1a2e3435f65c6e2721157dcf6065c5af7c
coati-00 <coati.00@gmail.com>
2015-04-20 13:15:14 -0400

    update

--- media/js/controllers.js
+++ media/js/controllers.js
@@ -6,20 +6,26 @@ TodoControllers.controller('TodoListCtrl', ['$scope', '$dragon', function ($scop
     $scope.channel = 'todos';
 
     $dragon.onReady(function() {
+    	console.log("onReady executes only once, when the connection is ready"); 
         $dragon.subscribe('todo-item', $scope.channel, {todo_list__id: 1}).then(function(response) {
             $scope.dataMapper = new DataMapper(response.data);
         });
 
         $dragon.getSingle('todo-list', {id:1}).then(function(response) {
+        	console.log(response.data);
             $scope.todoList = response.data;
         });
 
         $dragon.getList('todo-item', {list_id:1}).then(function(response) {
+        	console.log(response.data);
             $scope.todoItems = response.data;
         });
     });
 
     $dragon.onChannelMessage(function(channels, message) {
+    	console.log("onChannelMessage triggered every time some data is published.");
+    	console.log(channels); 
+    	console.log(message); 
         if (indexOf.call(channels, $scope.channel) > -1) {
             $scope.$apply(function() {
                 $scope.dataMapper.mapData($scope.todoItems, message);
@@ -37,3 +43,15 @@ console.log(TodoControllers.controller());
 console.log(TodoControllers.controller().TodoListCtrl);
 console.log(TodoControllers.controller());
 console.log("End JS");
+
+swampdragon.callRouter('todo-item', {'done': true}, function (context, data) {
+    console.log(data.result);
+});
+
+swampdragon.getSingle('todo-list', {id:'1'}, function (context, data) {
+	console.log(data);
+}, function (context, data) {console.log(data);
+});
+
+var data = {some_value: 12, other_value: 'hello world', id: 123};
+swampdragon.update('todo-list', data, function (context, data) { console.log(data); }, function (context, data) { console.log(data); } );
